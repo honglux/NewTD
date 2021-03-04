09fe3895393a763d104fb0487b38ef4868a37b6f
Chen Zhao <cxz349@case.edu>
2021-03-04 14:57:20 -0500

    unit class
    unit empty class

---
+++ Source/NewTD/NewTD.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NewTD.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NewTD, "NewTD" );
