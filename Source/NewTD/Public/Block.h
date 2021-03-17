c28fcd5e30fc2fb3df1811a99280fd2437e1e74d
Chen Zhao <cxz349@case.edu>
2021-03-17 11:29:20 -0400

    BP Class

---
+++ Source/NewTD/Public/Block.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tower.h"
+#include "Block.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWTD_API ABlock : public ATower
+{
+	GENERATED_BODY()
+public:
+	ABlock();
+};
